3cbaa5a7b6dc5748b70b6d4f71b1e8a132b71032
Terence Jee <terence.jee@gov.ab.ca>
2023-05-27 11:13:59 -0600

    Version update

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-// export * from './auth/authentication-service';
-// export * from './auth/admin-authentication-service';
-// export * from './auth/keycloak-initializer';
-export * from './authentication-service.module';
