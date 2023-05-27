a4c0fd03f0504c48180cd3a121f492a191c2a678
Terence Jee <terence.jee@gov.ab.ca>
2023-05-27 11:33:32 -0600

    Add constructor for initializer

---
+++ src/authentication-service.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { AuthenticationService } from './auth/authentication-service';
+import { KeycloakInitializer } from './auth/keycloak-initializer';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  providers: [AuthenticationService, KeycloakService, KeycloakAngularModule],
+  exports: [], // Add all exported artifacts here
+})
+export class AuthenticationServiceModule {
+  constructor(keycloakService: KeycloakService) {
+    // Create an instance of KeycloakInitializer and initialize it
+    const initializer = new KeycloakInitializer(keycloakService, /* constructor arguments */);
+    initializer.initialize().then(() => {
+      // Keycloak initialized successfully
+    }).catch(() => {
+      // Failed to initialize Keycloak
+    });
+  }
+}
