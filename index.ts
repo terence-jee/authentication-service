88448678b5a07c098d958eef41724608323b22bf
Terence Jee <terence.jee@gov.ab.ca>
2023-05-27 11:27:12 -0600

    Bugfix

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 export * from './src/auth/authentication-service';
 export * from './src/auth/admin-authentication-service';
 export * from './src/auth/keycloak-initializer';
-export * from './src/authentication-service.module';
+// export * from './src/authentication-service.module';
