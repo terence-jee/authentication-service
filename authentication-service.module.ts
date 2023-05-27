7f95b2c088a3025e787dd79ae1639bd0095d0bc6
Terence Jee <terence.jee@gov.ab.ca>
2023-05-27 11:51:34 -0600

    Try fix

--- authentication-service.module.ts
+++ authentication-service.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { AuthenticationService } from './src/auth/authentication-service';
-import { KeycloakInitializer } from './src/auth/keycloak-initializer';
+// import { KeycloakInitializer } from './src/auth/keycloak-initializer';
 import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
 
 @NgModule({
@@ -9,14 +9,4 @@ import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
   providers: [AuthenticationService, KeycloakService, KeycloakAngularModule],
   exports: [], // Add all exported artifacts here
 })
-export class AuthenticationServiceModule {
-  constructor(keycloakService: KeycloakService) {
-    // Create an instance of KeycloakInitializer and initialize it
-    const initializer = new KeycloakInitializer(keycloakService, /* constructor arguments */);
-    initializer.initialize().then(() => {
-      // Keycloak initialized successfully
-    }).catch(() => {
-      // Failed to initialize Keycloak
-    });
-  }
-}
+export class AuthenticationServiceModule {}
