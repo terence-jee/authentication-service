5ee01f7517b64d7e0124e2ca3b16d5105fbc97c8
Terence Jee <terence.jee@gov.ab.ca>
2023-05-27 11:06:40 -0600

    Try update module

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export * from './auth/authentication-service';
-export * from './auth/admin-authentication-service';
-export * from './auth/keycloak-initializer';
+// export * from './auth/authentication-service';
+// export * from './auth/admin-authentication-service';
+// export * from './auth/keycloak-initializer';
+export * from './authentication-service.module';
